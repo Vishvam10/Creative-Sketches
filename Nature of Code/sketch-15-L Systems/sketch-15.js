afd2848c4476ea811673246768fd21fbc251a5f2
Vishvam <s.vishvam1025@gmail.com>
2022-04-24 23:31:55 +0530

    Added Tweakpane for L Systems

--- Nature of Code/sketch-15-L Systems/sketch-15.js
+++ Nature of Code/sketch-15-L Systems/sketch-15.js
@@ -1,3 +1,60 @@
+const PARAMS = {
+    dec_fact: 0.6,
+    angle: 25,
+    generation: 0,
+    color: {
+        r: 255,
+        g: 217,
+        b: 81,
+        a: 0.43,
+    },
+    line_wt: 0.5,
+    pause: false,
+};
+
+const createPane = () => {
+    const controlContainer = document.getElementById("controls");
+    const pane = new Tweakpane.Pane({
+        container: controlContainer,
+    });
+
+    const folder1 = pane.addFolder({
+        title: "Initialize",
+    });
+    const folder2 = pane.addFolder({
+        title: "General",
+    });
+
+    folder1
+        .addButton({
+            title: "Next",
+            label: "Generation",
+        })
+        .on("click", () => {
+            PARAMS.generation += 1;
+            updateGeneration();
+        });
+    folder1.addMonitor(PARAMS, "generation");
+
+    folder1.addInput(PARAMS, "angle", {
+        min: -180,
+        max: 180,
+    });
+    folder1.addInput(PARAMS, "dec_fact", {
+        min: 0.01,
+        max: 0.99,
+        step: 0.001,
+    });
+    folder1.addInput(PARAMS, "color");
+    folder1.addInput(PARAMS, "line_wt", {
+        min: 0.1,
+        max: 8,
+        step: 0.01,
+    });
+
+    folder2.addInput(PARAMS, "pause");
+};
+
 var angle;
 var axiom = "F";
 var sentence = axiom;
@@ -9,8 +66,8 @@ rules[0] = {
     b: "FF+[+F-F-F]-[-F+F+F]",
 };
 
-function generate() {
-    len *= 0.5;
+function updateGeneration() {
+    len *= PARAMS.dec_fact;
     var nextSentence = "";
     for (var i = 0; i < sentence.length; i++) {
         var current = sentence.charAt(i);
@@ -31,13 +88,15 @@ function generate() {
 }
 
 function turtle() {
-    background(51);
     resetMatrix();
     translate(width / 2, height);
-    stroke(255, 100);
+    strokeWeight(PARAMS.line_wt);
+    let c = `rgba(${parseInt(PARAMS.color.r)}, ${parseInt(
+    PARAMS.color.g
+  )}, ${parseInt(PARAMS.color.b)}, ${round(PARAMS.color.a, 2)})`;
+    stroke(c);
     for (var i = 0; i < sentence.length; i++) {
         var current = sentence.charAt(i);
-
         if (current == "F") {
             line(0, 0, 0, -len);
             translate(0, -len);
@@ -54,13 +113,14 @@ function turtle() {
 }
 
 function setup() {
+    createPane();
     width = windowWidth - 400;
     height = windowHeight - 50;
     createCanvas(width, height);
-    angle = radians(25);
-    background(51);
-    createP(axiom);
+}
+
+function draw() {
+    angle = radians(PARAMS.angle);
+    background(0);
     turtle();
-    var button = createButton("generate");
-    button.mousePressed(generate);
 }
