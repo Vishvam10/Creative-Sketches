f8f287f4d5c46cc4eed95f4734ddabd484264f38
Vishvam <s.vishvam1025@gmail.com>
2022-05-19 21:38:25 +0530

    Added Tweakpane

--- Nature of Code/sketch-30-Genetic Algorithms IV/sketch-30.js
+++ Nature of Code/sketch-30-Genetic Algorithms IV/sketch-30.js
@@ -1,8 +1,13 @@
 const PARAMS = {
-    ref_rate: 3,
-    max_pop: 200,
-    mut_rate: 0.01,
+    n_vehicles: 3,
+    n_food: 10,
+    n_poison: 10,
+    mut_rate: 0.1,
+    repr_rate: 10,
+    food_freq: 0.01,
+    pois_freq: 0.01,
     avg_fitness: 0,
+    show_perc: false,
     limit: 20,
     frameRate: 0,
     pause: false,
@@ -12,6 +17,10 @@ var foods = [];
 var poisons = []
 var vehicles = []
 var vehicle;
+var nv = 2,
+    np = 10,
+    nf = 10;
+
 
 const createPane = () => {
     const controlContainer = document.getElementById("controls");
@@ -25,58 +34,59 @@ const createPane = () => {
     const folder2 = pane.addFolder({
         title: "General",
     })
-    folder1.addBlade({
-        view: 'list',
-        label: 'fit_func',
-        options: [
-            { text: 'linear', value: 'linear' },
-            { text: 'quadratic', value: 'quad' },
-            { text: 'cubic', value: 'cubic' },
-            { text: 'bi-quadratic', value: 'biquad' },
-            { text: 'exponential', value: 'exp' },
-        ],
-        value: 'linear',
-    }).on("change", (ev) => {
-        clear();
-        fitness_function = ev.value;
-        intialize();
-    })
-    folder1.addInput(PARAMS, "ref_rate", {
+
+    folder1.addInput(PARAMS, "n_vehicles", {
         min: 1,
-        max: 100,
+        max: 10,
         step: 1
-    })
-    folder1.addBlade({
-        view: 'text',
-        label: 'name',
-        parse: (v) => String(v),
-        value: 'To be or not to be',
     }).on("change", (ev) => {
         clear();
-        target = str(ev.value)
-        intialize()
+        background(44);
+        intialize(ev.value, PARAMS.n_food, PARAMS.n_poison);
     })
-    folder1.addInput(PARAMS, "max_pop", {
-        min: 20,
-        max: 1000,
+    folder1.addInput(PARAMS, "n_food", {
+        min: 1,
+        max: 500,
         step: 1
     }).on("change", (ev) => {
-        popmax = ev.value
-        intialize()
+        clear();
+        background(44);
+        intialize(PARAMS.n_vehicles, ev.value, PARAMS.n_poison);
     })
+    folder1.addInput(PARAMS, "n_poison", {
+        min: 1,
+        max: 500,
+        step: 1
+    }).on("change", (ev) => {
+        clear();
+        background(44);
+        intialize(PARAMS.n_vehicles, PARAMS.n_food, ev.value);
+    })
+
     folder1.addInput(PARAMS, "mut_rate", {
         min: 0.001,
         max: 1,
         step: 0.001
     }).on("change", (ev) => {
-        mutationRate = ev.value
-        intialize()
+        intialize();
     })
-    folder1.addInput(PARAMS, "limit", {
-        min: 2,
-        max: 100,
-        step: 1
+    folder1.addInput(PARAMS, "repr_rate", {
+        min: 0.01,
+        max: 5,
+        step: 0.0001
     })
+    folder1.addInput(PARAMS, "food_freq", {
+        min: 0.001,
+        max: 1,
+        step: 0.0001
+    })
+    folder1.addInput(PARAMS, "pois_freq", {
+        min: 0.001,
+        max: 1,
+        step: 0.0001
+    })
+    folder2.addInput(PARAMS, "show_perc");
+
     folder2.addMonitor(PARAMS, "frameRate", {
         view: "graph",
         min: 1,
@@ -85,31 +95,24 @@ const createPane = () => {
     folder2.addMonitor(PARAMS, "frameRate", {
         label: "",
     });
-    folder2.addMonitor(PARAMS, "avg_fitness", {
-        view: "graph",
-        min: 0,
-        max: 1,
-    });
-    folder2.addMonitor(PARAMS, "avg_fitness", {
-        label: "",
-    });
+
     folder2.addInput(PARAMS, "pause");
 
 }
 
-const intialize = () => {
-    for (let i = 0; i < 2; i++) {
+const intialize = (nv = 2, nf = 10, np = 10) => {
+    for (let i = 0; i < nv; i++) {
         let x = random(width);
         let y = random(height);
-        vehicle = new Vehicle(x, y, random(0, 10), random(0, 10), 4);
+        vehicle = new Vehicle(x, y, random(0, 10), random(0, 10), 4, mutationRate = PARAMS.mut_rate);
         vehicles.push(vehicle);
     }
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < nf; i++) {
         let x = random(width);
         let y = random(height);
         foods.push(createVector(x, y));
     }
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < np; i++) {
         let x = random(width);
         let y = random(height);
         poisons.push(createVector(x, y));
@@ -129,13 +132,13 @@ function draw() {
         return
     }
     background(44)
-    if (random(1) < 0.1) {
+    if (random(1) < PARAMS.food_freq) {
         var x = random(width);
         var y = random(height);
         foods.push(createVector(x, y));
     }
 
-    if (random(1) < 0.01) {
+    if (random(1) < PARAMS.pois_freq) {
         var x = random(width);
         var y = random(height);
         poisons.push(createVector(x, y));
@@ -154,12 +157,12 @@ function draw() {
     }
 
     for (var i = vehicles.length - 1; i >= 0; i--) {
-        vehicles[i].boundaries();
+        vehicles[i].wrap();
         vehicles[i].behaviours(foods, poisons);
         vehicles[i].update();
-        vehicles[i].show();
+        vehicles[i].show(PARAMS.show_perc);
 
-        var newVehicle = vehicles[i].clone();
+        var newVehicle = vehicles[i].clone(PARAMS.repr_rate / 1000);
         if (newVehicle != null) {
             vehicles.push(newVehicle);
         }
--- Nature of Code/sketch-30-Genetic Algorithms IV/vehicle.js
+++ Nature of Code/sketch-30-Genetic Algorithms IV/vehicle.js
@@ -1,23 +1,22 @@
-var mutationRate = 0.1
-
 class Vehicle {
-    constructor(x, y, max_speed, max_force, size, color = "#ffffff", dna = [1, 1, random(1, 100), random(1, 100)]) {
+    constructor(x, y, max_speed, max_force, size, color = "#ffffff", dna = [1, 1, random(1, 100), random(1, 100)], mutationRate = 0.1) {
         this.pos = createVector(x, y);
         this.vel = createVector(0, 0);
         this.acc = createVector(0, 0);
         this.maxSpeed = max_speed;
         this.maxForce = max_force;
+        this.mutationRate = mutationRate;
         this.dna = dna
 
-        // for (let i = 0; i < dna.length; i++) {
-        //     if (random(1) < mutationRate) {
-        //         if (i > 1) {
-        //             this.dna[i] += random(-10, 10)
-        //         } else {
-        //             this.dna[i] += random(-0.1, 0.1)
-        //         }
-        //     }
-        // }
+        for (let i = 0; i < dna.length; i++) {
+            if (random(1) < this.mutationRate) {
+                if (i > 1) {
+                    this.dna[i] += random(-10, 10)
+                } else {
+                    this.dna[i] += random(-0.1, 0.1)
+                }
+            }
+        }
         // this.dna[0] = random(-2, 2); // Food Weight
         // this.dna[1] = random(-2, 2); // Poision Weight
         // this.dna[2] = random(1, 100); // Food Perception : like a radius
@@ -78,10 +77,10 @@ class Vehicle {
         this.vel.add(this.acc);
         this.vel.limit(this.maxSpeed);
         this.pos.add(this.vel);
-        this.acc.mult(0);
+        this.acc.set(0, 0);
     }
 
-    show() {
+    show(show_perc) {
 
         var angle = this.vel.heading() + PI / 2;
 
@@ -89,32 +88,34 @@ class Vehicle {
         translate(this.pos.x, this.pos.y);
         rotate(angle);
 
-        strokeWeight(3);
-        stroke(0, 255, 0);
-        noFill();
-        line(0, 0, 0, -this.dna[0] * 20);
+        if (show_perc) {
 
-        noFill();
-        strokeWeight(0.5);
-        ellipse(0, 0, this.dna[2] * 2);
+            strokeWeight(3);
+            stroke(0, 255, 0);
+            noFill();
+            line(0, 0, 0, -this.dna[0] * 20);
+            strokeWeight(0.5);
+            ellipse(0, 0, this.dna[2] * 2);
 
-        strokeWeight(2);
-        strokeWeight(0.5);
-        ellipse(0, 0, this.dna[2] * 2);
+            strokeWeight(2);
+            strokeWeight(0.5);
+            ellipse(0, 0, this.dna[2] * 2);
 
-        stroke(255, 0, 0);
-        noFill();
-        ellipse(0, 0, this.dna[3] * 2);
+            stroke(255, 0, 0);
+            noFill();
+            ellipse(0, 0, this.dna[3] * 2);
+            noFill();
 
-        strokeWeight(3);
-        stroke(0, 255, 0);
-        noFill();
-        line(0, 0, 0, -this.dna[1] * 20);
+            strokeWeight(3);
+            stroke(0, 255, 0);
+            noFill();
+            line(0, 0, 0, -this.dna[1] * 20);
+        }
 
         fill(255);
         noStroke();
 
-        let green = color(0, 255, 0);
+        let green = color(255, 255, 255);
         let red = color(255, 0, 0);
         let col = lerpColor(red, green, this.health);
         // let col = color(255, 255, 255)
@@ -174,9 +175,9 @@ class Vehicle {
 
     }
 
-    clone() {
-        if (random(1) < 0.001) {
-            return new Vehicle(this.pos.x, this.pos.y, this.maxSpeed, this.maxForce, this.dna)
+    clone(repr_rate = 0.001) {
+        if (random(1) < repr_rate) {
+            return new Vehicle(this.pos.x, this.pos.y, this.maxSpeed, this.maxForce, this.dna, this.mutationRate)
         }
     }
 
